b3c44ef6b83990d331105f8ad88ecd7d24d0f63c
Raj kelawala <rajkelawala28@gmail.com>
2025-03-18 11:08:47 +0530

    Add poll creation functionality with form handling and update templates

--- poll/views.py
+++ poll/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from .forms import CreatePollForm
 
 def home(request):
     context = {}
     return render(request, 'home.html', context)
 
 def create(request):
-    context = {}
+    form = CreatePollForm()  # Initialize form here
+
+    if request.method == 'POST':
+        form = CreatePollForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # Redirect inside the if block
+
+    context = {'form': form}
     return render(request, 'create.html', context)
 
 def results(request):
--- poll_app/settings.py
+++ poll_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "poll",
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = "poll_app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'poll_app',
+        'USER': 'root',
+        'HOST': '127.0.0.1',
+        'PASSWORD': 'raj_28092003',
+        'PORT': '3306',
     }
 }
 
