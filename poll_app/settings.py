80df4da9f6abf687db2f31b4b5b8d1de40ef4233
Raj kelawala <rajkelawala28@gmail.com>
2025-03-19 10:19:42 +0530

    Update settings for deployment and add Vercel configuration

--- poll_app/settings.py
+++ poll_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure--ffp@xa^_d*m8_l4qtzytcm10go^^n)m*am+lklki7$fz=zvil
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'poll_app',
         'USER': 'root',
         'HOST': '127.0.0.1',
-        'PASSWORD': 'Your Password',
+        'PASSWORD': 'raj_28092003',
         'PORT': '3306',
     }
 }
